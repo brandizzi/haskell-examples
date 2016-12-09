e9ab62511f1ce09b3a74946913b645a3ef13f901
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2016-12-09 05:59:25 -0300

    using foldl

--- revpolish3.hs
+++ revpolish3.hs
@@ -1,16 +1,22 @@
 main = do
   c <- getContents
-  putStrLn $ present c
+  putStrLn $ unwords . map show . interpret' $ c
 
+stack :: (Floating a, Read a) => String -> [a] -> [a]
 stack "+" (x:x':xs) = (x+x'):xs
-stack "-" (x:x':xs) = (x-x'):xs
+stack "-" (x:x':xs) = (x'-x):xs
 stack "*" (x:x':xs) = (x*x'):xs
-stack "/" (x:x':xs) = (x `quot` x'):xs
-stack s xs = (read s :: Integer):xs
+stack "/" (x:x':xs) = (x'/ x):xs
+stack "ln" (x:xs) = (log x):xs
+stack s xs = (read s):xs
 
 collapse [] = []
 collapse (x:xs) = stack x (collapse xs)
 
+collapse' = foldl (flip stack) []
+
+
 parse = reverse . words
 interpret = collapse . parse
+interpret' = collapse' . words
 present = unwords . map show . interpret
